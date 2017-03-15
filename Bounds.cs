7f3277439555c991201f4b9670b822ff06409b80
donhau1811 <trandonhau1811@gmail.com>
2017-03-15 23:36:40 +0200

    Add files via upload
    Camera Scripts

---
+++ Bounds.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounds : MonoBehaviour {
+
+    private BoxCollider2D bounds;
+    private CameraController theCamera;
+    // Use this for initialization
+	void Start () {
+        bounds = GetComponent<BoxCollider2D>();
+        theCamera = FindObjectOfType<CameraController>();
+        theCamera.setBounds(bounds);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
