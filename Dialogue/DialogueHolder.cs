1205e20a5e15e472620c2d12a786f404afc2adb6
donhau1811 <trandonhau1811@gmail.com>
2017-03-16 00:18:35 +0200

    Add files via upload
    Dialogue Scripts

---
+++ Dialogue/DialogueHolder.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueHolder : MonoBehaviour {
+
+    public string dialogue;
+    private DialogueManager dMan;
+
+    public string[] dialogueLines;
+
+    bool inTrigger;
+	// Use this for initialization
+	void Start () {
+        dMan = FindObjectOfType<DialogueManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyUp(KeyCode.Space) && inTrigger)
+        {
+            //dMan.showBox(dialogue);
+            if (!dMan.dialogActive)
+            {
+                dMan.dialogLines = dialogueLines;
+                dMan.currentLine = 0;
+                dMan.showDialog();
+            }
+
+            //if (transform.parent.GetComponent<EvilController>() != null)
+            //{
+            //    transform.parent.GetComponent<EvilController>().canMove = false;
+            //}
+        }
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            inTrigger = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            inTrigger = false;
+        }
+    }
+}
---
+++ Dialogue/DialogueManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+
+    public GameObject dBox;
+    public Text dText;
+
+    public bool dialogActive;
+
+    public string[] dialogLines;
+    public int currentLine;
+
+    private PlayerController thePlayer;
+
+    public bool keyboardAllow; //only for level 2-2
+    public bool checkIfCorrectQuest; //only for level 2-2
+	// Use this for initialization
+	void Start () {
+        thePlayer = FindObjectOfType<PlayerController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (dialogActive && Input.GetKeyUp(KeyCode.Return))
+        {
+            //dBox.SetActive(false);
+            //dialogActive = false;
+            currentLine++;
+        }
+
+        if (currentLine >= dialogLines.Length)
+        {
+            dBox.SetActive(false);
+            dialogActive = false;
+
+            currentLine = 0;
+
+            thePlayer.canMove = true;
+
+            if (checkIfCorrectQuest)
+                keyboardAllow = true;
+        }
+
+        dText.text = dialogLines[currentLine];
+	}
+
+    //public void showBox(string dialog)
+    //{
+    //    dialogActive = true;
+    //    dBox.SetActive(true);
+    //    dText.text = dialog;    
+    //}
+
+    public void showDialog()
+    {
+        dialogActive = true;
+        dBox.SetActive(true);
+        thePlayer.canMove = false;
+    }
+}
