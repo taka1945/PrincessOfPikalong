2fcb569b020645e82191d4dc03b9e1f72a944999
donhau1811 <trandonhau1811@gmail.com>
2017-03-16 00:22:30 +0200

    Add files via upload
    Audio Scripts

---
+++ Audio/MusicSwitcher.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicSwitcher : MonoBehaviour {
+
+    private MusicController theMC;
+
+    public int newTrack;
+
+    public bool switchOnStart;
+
+	// Use this for initialization
+	void Start () {
+        theMC = FindObjectOfType<MusicController>();
+
+        if (switchOnStart)
+        {
+            theMC.switchTrack(newTrack);
+            gameObject.SetActive(false);
+        }
+
+        if (PlayerPrefs.GetInt("loadFromMain") != 0)
+        {
+            theMC.switchTrack(newTrack);
+            PlayerPrefs.SetInt("loadFromMain", 0);
+            gameObject.SetActive(false);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            theMC.switchTrack(newTrack);
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Audio/VolumeManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeManager : MonoBehaviour {
+
+    public VolumeController[] vcObjects;
+
+    public float maxVolumeLevel = 1.0f;
+    public float currentVolumeLevel;
+
+    private static bool vcExists;
+    // Use this for initialization
+	void Start () {
+        vcObjects = FindObjectsOfType<VolumeController>();
+
+        if (!vcExists)
+        {
+            vcExists = true;
+            DontDestroyOnLoad(transform.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        if (currentVolumeLevel > maxVolumeLevel)
+        {
+            currentVolumeLevel = maxVolumeLevel;
+        }
+
+        for (int i = 0; i < vcObjects.Length; i++)
+        {
+            vcObjects[i].setAudioLevel(currentVolumeLevel);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
